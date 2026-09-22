f7e3710ea6e10b6c604dd104cf591c0c5611a777
Anmoldeep Sandhu <adeepsandhu2007@gmail.com>
2026-09-22 18:30:00 +0530

    cprogramming

---
+++ calculator.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    char op;
+    float a, b, res;
+    printf("Enter the operator(+,-,*,/) : ");
+    scanf("%c", &op);
+    printf("Enter two operands : ");
+    scanf("%f %f", &a, &b);
+}
