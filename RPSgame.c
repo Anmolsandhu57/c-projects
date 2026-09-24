6b1763f352596bd4336c4ef112cde6d09405f30b
Anmoldeep Sandhu <adeepsandhu2007@gmail.com>
2026-09-24 22:42:54 +0530

    game

---
+++ RPSgame.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main(){
+    int user, computer;
+    printf("=====Rock Paper Sessiors====\n");
+    printf("\n1. Rock");
+    printf("\n2. Paper");
+    printf("\n3. Sessiors");
+    printf("Enter your choice : ");
+    scanf("%d", &user);
+    return 0;
+}
