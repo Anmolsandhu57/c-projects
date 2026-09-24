226089922d049ac469be4490348779ae6a95180b
Anmoldeep Sandhu <adeepsandhu2007@gmail.com>
2026-09-24 21:56:59 +0530

    calculator

--- calculator.c
+++ calculator.c
@@ -6,4 +6,24 @@ int main(){
     scanf("%c", &op);
     printf("Enter two operands : ");
     scanf("%f %f", &a, &b);
+    switch(op){
+    case '+' :
+    res = a + b;
+    break;
+    case '-' :
+    res = a - b;
+    break;
+    case '*' :
+    res = a * b;
+    break;
+    case '/' :
+    res = a / b;
+    break;
+    default:
+    printf("Invalid Operator");
+    return 0;
+    }
+    printf("%f", res);
+    return 0;
+
 }
