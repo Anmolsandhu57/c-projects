05387e4221fb7672b44f04507dd62bd9edb30325
Anmoldeep Sandhu <adeepsandhu2007@gmail.com>
2026-09-25 11:44:52 +0530

    game

--- RPSgame.c
+++ RPSgame.c
@@ -9,5 +9,26 @@ int main(){
     printf("\n3. Sessiors");
     printf("Enter your choice : ");
     scanf("%d", &user);
+
+    srand(time(0));
+    computer = rand( ) %3 + 1;
+
+    printf("computer chose : %d\n", computer);
+
+    if(user == computer){
+        printf("It's a draw!\n");
+    }
+    else if(user == 1 && computer == 3){
+        printf("Yoy win!\n");
+    }
+    else if(user == 2 && computer == 1){
+        printf("You win!\n");
+    }
+    else if(user == 3 && computer == 2){
+        printf("You win!\n");
+    }
+    else{
+        printf("Computer wins!\n");
+    }
     return 0;
 }
